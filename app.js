5be151c60986665c17aec8a4556781e91fdc23fa
Bharath Kalyan <bharath.kalyan@nagarro.com>
2023-08-29 17:52:49 +0530

    Custom PORT

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
-const port = 8080;
 
+require('dotenv').config();
+
+const port = process.env.PORT || 8080;
+console.log("PORT USED IS :: " +  process.env.PORT);
 const BASE_URL = "https://jsonplaceholder.typicode.com/";
 
 app.get("/", (req, res) => {
