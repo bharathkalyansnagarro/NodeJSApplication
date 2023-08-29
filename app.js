61df850f49ef4c695d425923f72b583877d6dd9b
Bharath Kalyan <bharath.kalyan@nagarro.com>
2023-08-29 17:18:11 +0530

    Changed PORT to 80

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
-const port = 3000;
+const port = 80;
 
 const BASE_URL = "https://jsonplaceholder.typicode.com/";
 
